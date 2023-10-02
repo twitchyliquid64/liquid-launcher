ad3431bbc120778e22ae0303f3cb315eca6b91fa
Tom <twitchyliquid64@ciphersink.net>
2023-10-01 23:43:13 -0700

    Display system apps

--- src/app.rs
+++ src/app.rs
@@ -3,6 +3,9 @@ use std::collections::HashMap;
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
 
+use fuzzy_matcher::skim::SkimMatcherV2;
+use fuzzy_matcher::FuzzyMatcher;
+
 const ICON_SIZE: [usize; 2] = [32, 32];
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
@@ -17,9 +20,15 @@ pub struct Launcher {
     #[serde(skip)]
     applications: Arc<Mutex<Option<crate::sys_apps::AppList>>>,
     #[serde(skip)]
+    matching_app_idx: Option<Vec<usize>>,
+    #[serde(skip)]
+    selected_idx: Option<usize>,
+    #[serde(skip)]
     icons: Arc<Mutex<HashMap<String, PathBuf>>>,
     #[serde(skip)]
     empty_icon: Option<egui::load::SizedTexture>,
+    #[serde(skip)]
+    matcher: SkimMatcherV2,
 }
 
 impl Default for Launcher {
@@ -31,6 +40,9 @@ impl Default for Launcher {
             applications: Arc::new(Mutex::new(None)),
             icons: Arc::new(Mutex::new(HashMap::new())),
             empty_icon: None,
+            matcher: SkimMatcherV2::default(),
+            matching_app_idx: None,
+            selected_idx: None,
         }
     }
 }
@@ -75,8 +87,8 @@ impl Launcher {
                                 (*data).insert(app.name.clone(), path);
                             }
                         }
-                        // request redraw after every 8 entries
-                        if i % 8 == 0 {
+                        // request redraw after every 12 entries
+                        if i % 12 == 0 {
                             frame.request_repaint();
                         }
                     }
@@ -101,9 +113,53 @@ impl Launcher {
         }
     }
 
+    fn handle_input_changed(&mut self) {
+        let matcher = SkimMatcherV2::default();
+        self.matching_app_idx = match &*self.applications.lock().unwrap() {
+            None => None,
+            Some(apps_list) => {
+                if self.input.len() == 0 {
+                    Some(
+                        apps_list
+                            .apps
+                            .iter()
+                            .enumerate()
+                            .map(|(i, _app)| i)
+                            .collect(),
+                    )
+                } else {
+                    let mut idx_scores: Vec<(usize, i64)> = apps_list
+                        .apps
+                        .iter()
+                        .enumerate()
+                        .map(
+                            |(i, app)| match matcher.fuzzy_match(&app.name, &self.input) {
+                                Some(score) => Some((i, score)),
+                                None => None,
+                            },
+                        )
+                        .filter(|e| e.is_some())
+                        .map(|e| e.unwrap())
+                        .collect();
+
+                    idx_scores.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
+
+                    Some(idx_scores.into_iter().map(|e| e.0).collect())
+                }
+            }
+        };
+
+        if let Some(idx) = self.selected_idx {
+            if self.matching_app_idx.as_ref().map(|v| v.len()).unwrap_or(0) <= idx {
+                self.selected_idx = Some(0);
+            }
+        }
+    }
+
     fn ui_for_app_entry(
         &self,
         app: &crate::sys_apps::App,
+        selected: bool,
         ctx: &egui::Context,
         ui: &mut egui::Ui,
         icons: &HashMap<String, PathBuf>,
@@ -119,7 +175,7 @@ impl Launcher {
             ui.add(egui::Image::new(ImageSource::Texture(tex_info)));
         }
 
-        ui.label(app.name.clone());
+        ui.selectable_label(selected, app.name.clone());
     }
 }
 
@@ -131,9 +187,6 @@ impl eframe::App for Launcher {
 
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             ui.allocate_space(egui::Vec2::new(0., 4.));
             ui.horizontal(|ui| {
@@ -149,28 +202,62 @@ impl eframe::App for Launcher {
                     self.focus_input = false;
                     input.request_focus();
                 }
+
+                let (down, up) = if input.has_focus() {
+                    ui.input(|i| {
+                        (
+                            i.key_pressed(egui::Key::ArrowDown),
+                            i.key_pressed(egui::Key::ArrowUp),
+                        )
+                    })
+                } else {
+                    (false, false)
+                };
+
+                if input.changed() || down || up {
+                    self.handle_input_changed();
+                }
+                if down || up {
+                    let length = self.matching_app_idx.as_ref().map(|v| v.len()).unwrap_or(0);
+                    if length > 0 {
+                        if let Some(idx) = self.selected_idx.as_mut() {
+                            if down && idx.clone() < length - 1 {
+                                *idx += 1;
+                            } else if up && idx.clone() > 0 {
+                                *idx -= 1;
+                            }
+                        } else if down {
+                            self.selected_idx = Some(0);
+                        }
+                    } else {
+                        self.selected_idx = None;
+                    }
+                }
             });
             ui.allocate_space(egui::Vec2::new(0., 4.));
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
             {
+                let row_height = ui
+                    .text_style_height(&egui::TextStyle::Body)
+                    .min(ICON_SIZE[1] as f32);
+
                 let apps_mutex = &*self.applications.lock().unwrap();
                 let icons_mutex = &*self.icons.lock().unwrap();
-                match apps_mutex {
-                    Some(apps_list) => {
-                        ui.separator();
-
-                        let row_height = ui
-                            .text_style_height(&egui::TextStyle::Body)
-                            .min(ICON_SIZE[1] as f32);
+                match (apps_mutex, &self.matching_app_idx) {
+                    (Some(apps_list), Some(idx)) => {
                         egui::ScrollArea::vertical()
                             .auto_shrink([false; 2])
-                            .show_rows(ui, row_height, apps_list.apps.len(), |ui, row_range| {
+                            .show_rows(ui, row_height, idx.len(), |ui, row_range| {
                                 egui::Grid::new("apps_grid").num_columns(3).show(ui, |ui| {
                                     for row in row_range {
                                         self.ui_for_app_entry(
-                                            &apps_list.apps[row],
+                                            &apps_list.apps[idx[row]],
+                                            match self.selected_idx {
+                                                Some(i) => i == row,
+                                                None => false,
+                                            },
                                             ctx,
                                             ui,
                                             icons_mutex,
@@ -180,7 +267,29 @@ impl eframe::App for Launcher {
                                 });
                             });
                     }
-                    None => {}
+                    (Some(apps_list), None) => {
+                        egui::ScrollArea::vertical()
+                            .auto_shrink([false; 2])
+                            .show_rows(ui, row_height, apps_list.apps.len(), |ui, row_range| {
+                                egui::Grid::new("apps_grid").num_columns(3).show(ui, |ui| {
+                                    for row in row_range {
+                                        self.ui_for_app_entry(
+                                            &apps_list.apps[row],
+                                            match self.selected_idx {
+                                                Some(i) => i == row,
+                                                None => false,
+                                            },
+                                            ctx,
+                                            ui,
+                                            icons_mutex,
+                                        );
+                                        ui.end_row();
+                                    }
+                                });
+                            });
+                    }
+
+                    _ => {}
                 }
             }
 
@@ -201,7 +310,7 @@ fn configure_text_styles(ctx: &egui::Context) {
         (TextStyle::Body, FontId::new(20.0, Proportional)),
         (TextStyle::Monospace, FontId::new(16.0, Monospace)),
         (TextStyle::Button, FontId::new(16.0, Proportional)),
-        (TextStyle::Small, FontId::new(12.0, Proportional)),
+        (TextStyle::Small, FontId::new(15.0, Proportional)),
     ]
     .into();
     ctx.set_style(style);
